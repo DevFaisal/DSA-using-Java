7f83fe7e3e227f1a038e65849e3006878aceed07
Faisal Farooq <dfaisal59@gmail.com>
2022-12-18 17:15:20 +0530

    modified:   SortingAlgorithms.java

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,10 +1,31 @@
 public class SortingAlgorithms {
 
-    public static void BinarySort(int arr[]) {
+    public static void BubbleSort(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; i < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
 
     }
 
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
+
+        int arr[] = { 5, 4, 3, 2, 1 };
+        BubbleSort(arr);
+        printArray(arr);
 
     }
 }
