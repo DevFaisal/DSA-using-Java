1a6234bf83ee256bdd7c28002b9dbe918da5093a
Faisal Farooq <dfaisal59@gmail.com>
2023-08-09 09:08:53 +0530

    new files added

---
+++ src/Stack/stack.java
@@ -0,0 +1,23 @@
+package Stack;
+
+public class stack {
+    public class globals {
+        public static int top = -1;
+    }
+
+    public static void push(int data) {
+        
+       
+
+    }
+
+    public static void pop(int data) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int size = 10;
+        int stack[] = new int[size];
+    }
+}
--- src/index.java
+++ src/index.java
@@ -2,24 +2,18 @@ import java.util.*;
 
 public class index {
 
-	public static int missingNumber(int A[], int N) {
-
-		int sumOfArray = 0;
-
-		for (int i = 0; i < A.length-1; i++) {
-			sumOfArray += A[i];
-		}
-		int n = A.length;
-
-		int expectedSum = (n * (n + 1)) / 2;
-
-		return (expectedSum - sumOfArray);
-
-	}
-
 	public static void main(String[] args) {
 
-		int arr[] = { 2, 5, 3, 1 };
-		System.out.println(missingNumber(arr, arr.length));
+		int nums[] = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+
+		int val = 2;
+
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				nums[count] = nums[i];
+				count++;
+			}
+		}
 	}
 }
